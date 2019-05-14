9b05a0b415f26b5fa1afd12e8e8984de1fad3c9d
Fabian <fabian.tapia.bascur@gmail.com>
2019-05-13 21:58:40 -0400

    Desafios lunes poo modulos mixins y rack resueltos

--- poo_modulos_mixins_rack/presencial/desafio_lunes/7rack.rb
+++ poo_modulos_mixins_rack/presencial/desafio_lunes/7rack.rb
@@ -2,7 +2,7 @@
 require 'rack'
 class MiPrimeraWebApp
   def call(env)
-    [200, {'Content-Type => 'text/html'}, ['Lorem ipsum']]
+    [200, {'Content-Type' => 'text/html'}, ['<p>Lorem ipsum</p>']]
   end
 end
 run MiPrimeraWebApp.new
